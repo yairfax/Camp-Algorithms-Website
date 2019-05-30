86d20a91c452aca7484057120f69854de236fe80
Yair <yairshop@gmail.com>
2019-05-30 08:21:26 -0400

    minor update

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ app.delete("/chugim/klugie/:id", ensureLogin.ensureLoggedIn(), function(req, res
 	var session = req.params.id;
 	Session.findOneAndUpdate({_id: session, 'campers.name': req.query.camper}, {$pull:{campers:{name:req.query.camper}, noChug:{name:req.query.camper}}}, {select: {'campers.$': 1, _id: 0, tears: 1, lastProduction: 1}}, function(err, data) {
 		if (err) throw err;
-		if (!data) return res.send("please send a valid id");
+		if (!data) return res.send("please send a valid id or camper name");
 
 		if (data.lastProduction) { // case of chug list hasn't been produced yet
 			if (!data.campers[0].chug) { // case of kid you're editing didn't have a chug
@@ -376,7 +376,6 @@ app.delete("/chugim/klugie/:id", ensureLogin.ensureLoggedIn(), function(req, res
 
 			Session.findOneAndUpdate({_id: session, 'klugim.name': data.campers[0].chug}, {$pull:{'klugim.$.kids':{name:req.query.camper}}}, function(err, data0) {
 				if (err) throw err;
-				if (!data) return res.send("please send a valid id");
 
 				Session.findByIdAndUpdate(session, {tears: data.tears}, function(err, data) {
 					if (err) throw err;
